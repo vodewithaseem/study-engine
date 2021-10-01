ab0c006db1cc050eb13918566562c507ec2426b6
vodewithaseem <82634502+vodewithaseem@users.noreply.github.com>
2021-10-01 10:38:30 +0530

    Delete ste directory

--- ste/python/browzer.py
+++
@@ -1,5 +0,0 @@
-from PyQt5.QtWidgets import *
-
-class MainWindow(QMainWindow):
-    def __init__(self):
-        
--- ste/websit/js/rs.js
+++
@@ -1,6 +0,0 @@
-// ul = document.getElementById("myUL");
-// bar = document.querySelector(".myInput")
-
-// bar.addEventListener("keypress", (event) => {
-//    ul.classList.toggle(ul.s);
-// })
